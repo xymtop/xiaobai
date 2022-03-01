e7af4805b2de4a98174f24cd7ed93a3da02372c3
xymtop <writehnmy@vip.qq.com>
2022-03-01 22:49:34 +0800

    更新代码

--- mypet/src/com/xymtop/Poem.java
+++ mypet/src/com/xymtop/Poem.java
@@ -1,20 +1,30 @@
 package com.xymtop;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.Random;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.event.MouseInputAdapter;
+import com.xymtop.*;
 
 public class Poem {
     JFrame frame;
     JLabel label;
     static Point mouse = new Point();
+    Boolean ishide;
+    Boolean alw;
 
     public Poem() {
+        ishide = false;
+        alw = true;
         // 获得当前屏幕的宽度，
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int screenWidth = (int) screenSize.getWidth();
@@ -26,7 +36,7 @@ public class Poem {
 
         label = new JLabel(html);
         frame.resize(r.width, r.height);
-        label.setForeground(new Color(8, 46, 55));
+        label.setForeground(new Color(36, 187, 123));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // 框体透明
         frame.setUndecorated(true); // 取消窗口标题栏
@@ -114,7 +124,7 @@ public class Poem {
             }
 
         });
-
+        Opentray();
         frame.setVisible(true);
     }
 
@@ -126,6 +136,86 @@ public class Poem {
         String author = str[str.length - 8];
         String sub = str[str.length - 16];
         return content + "<br>---" + author + "《" + sub + "》";
+    }
+
+    // 系统托盘
+    private void Opentray() {
+        // 判断是否支持系统托盘
+        if (SystemTray.isSupported()) {
+            // 获取图片所在的URL,请把图片放在同一个包下
+            // URL url = this.getClass().getResource("res/static/mylogo.png");
+            // 实例化图像对象
+            ImageIcon icon = new ImageIcon("res/static/mylogo.jpg");
+            // 获得Image对象
+            Image image = icon.getImage();
+            // 创建托盘图标
+            TrayIcon trayIcon = new TrayIcon(image);
+            // 为托盘添加鼠标适配器
+            trayIcon.addMouseListener(new MouseInputAdapter() {
+                // 鼠标事件
+                public void mouseClicked(MouseEvent e) {
+                    // 判断是否双击了鼠标
+                    if (e.getClickCount() == 2) {
+                        if (ishide) {
+                            frame.setVisible(true);
+                        } else {
+                            frame.setLocation(0, 0);
+                        }
+
+                    }
+                }
+            });
+
+            // 添加工具提示文本
+            trayIcon.setToolTip("小野喵诗句,让你的桌面更优雅！");
+            // 创建弹出菜单
+            PopupMenu popupMenu = new PopupMenu();
+
+            popupMenu.add(new MenuItem("隐藏或者显示"));
+            popupMenu.add(new MenuItem("置顶窗口"));
+            // popupMenu.add(new MenuItem("关于"));
+            popupMenu.add(new MenuItem("退出"));
+
+            // 监听菜单点击事件
+            popupMenu.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // TODO Auto-generated method stub
+                    String cmd = e.getActionCommand();
+                    if (cmd == "退出") {
+                        System.exit(0);
+                    } else if (cmd == "隐藏或者显示") {
+                        if (ishide) {
+                            frame.setVisible(true);
+                            ishide = false;
+                        } else {
+                            frame.setVisible(false);
+                            ishide = true;
+                        }
+                    } else if (cmd == "置顶窗口") {
+                        frame.setAlwaysOnTop(!alw);
+                        alw = !alw;
+                    }
+                }
+
+            });
+
+            popupMenu.addSeparator();
+
+            // 为托盘图标加弹出菜弹
+            trayIcon.setPopupMenu(popupMenu);
+            // 获得系统托盘对象
+            SystemTray systemTray = SystemTray.getSystemTray();
+            try {
+                // 为系统托盘加托盘图标
+                systemTray.add(trayIcon);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "不支持桌面托盘");
+        }
     }
 
 }
